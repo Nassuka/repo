d21d32e36ae1025c2ce77cdbd348512b98b85f81
Nassuka <135132361+Nassuka@users.noreply.github.com>
2023-07-04 09:32:04 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,44 +14,7 @@ from fonctions import calcul_conso_gpl, calcul_energy_hfo, price_hfo, price_gpl,
 #from gen_pdf import gen_pdf
 
 #img = Image.open('Logo_TotalEnergies.png')
-"""def calcul_energy_hfo(conso):
-    return conso*11774
 
-def calcul_conso_gpl(energy):
-    return energy/13800
-
-def price_hfo(conso):
-    return conso*408.9
-
-def price_gpl(conso) : 
-    return conso*602
-
-def euro_to_dollar(eur):
-    return eur * 1.09
-
-def dollar_to_CFA(dollar):
-    return dollar * 600.72
-
-def dollar_to_ZAR(dollar): 
-    return dollar * 18.34
-
-def dollar_to_mur(dollar):
-    return dollar * 45.5
-
-def dollar_to_din_tun(dollar):
-    return dollar * 3.09
-
-def space_in_numbers(x):
-    n= ""
-    
-    for i in range(1,len(x) + 1):
-        if i%3 == 0 and i != len(x):
-            n = " " + x[-i] + n
-        else :
-            n = x[-i] + n
-    
-    return n
-    """
 
 #Icône et nom de l'onglet
 st.set_page_config(page_title='Comparaison HFO vs GPL', page_icon = 'Logo_TotalEnergies.png', initial_sidebar_state="expanded", layout = "wide")
